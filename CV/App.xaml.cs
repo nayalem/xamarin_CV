e90970546e7021e64c5d703c411b6b2a32c72d8e
nayalem <143141850+nayalem@users.noreply.github.com>
2024-06-25 00:55:42 -0500

    Add files via upload

---
+++ CV/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+
+namespace CVApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage(); // Aquí se inicializa la MainPage
+        }
+
+       
+
+    }
+}
+
+
---
+++ CV/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace CVApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
